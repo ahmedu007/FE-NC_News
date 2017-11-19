f9cf51eda8a1dc1bdea3e1de262fc6e163b3eb4e
Umair Ahmed <umair170@hotmail.com>
2017-11-19 18:37:11 +0000

    Updating logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,17 @@
 import React from "react";
 import Topic from "./Topics";
 
+import logo from "../NCN-black border cropped.png";
+
 class Navbar extends React.Component {
   render() {
     return (
       <header className="header">
         <h1>
           <img
-            src="https://tsuyoshiwada.github.io/react-stack-grid/images/logo.png"
-            // src="https://www.imageupload.co.uk/images/2017/11/14/Logomakr_1b6YJB.png"
-            alt="React Stack Grid"
+            src={logo}
+            //src="https://tsuyoshiwada.github.io/react-stack-grid/images/logo.png"
+            alt="NC News Logo"
           />
         </h1>
         <nav>

8d7095a9a922ce9a8149633522e3eb8ae19208e2
Umair Ahmed <umair170@hotmail.com>
2017-12-17 19:25:03 +0000

    Fixing not found component not rendering

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,13 +1,13 @@
 import React from "react";
 import img from "./404.gif";
 
-const NotFound = () => {
+const NotFound = props => {
   return (
     <div>
       <article style={{ color: "black", fontSize: "1.6rem" }}>
         The requested URL
         <strong>
-          <em> {this.props.location.pathname} </em>
+          <em> {props.location.pathname} </em>
         </strong>
         was not found on this server.
       </article>

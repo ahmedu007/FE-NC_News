f28660e986dc17c8b9a9b36e9144a5baf62e2c36
ahmedu007 <umair170@hotmail.com>
2018-01-12 03:08:37 +0000

    Adding a footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,49 +1,27 @@
 import React from "react";
+import { Segment, Container, Divider, List, Icon } from "semantic-ui-react";
 
 const Footer = () => {
   return (
-    <footer className="footer">
-      <nav>
-        <ul>
-          <li>
-            <a href="https://twitter.com/umair170">
-              <i
-                className="fa fa-twitter icon-2x"
-                aria-hidden="true"
-                title="Twitter"
-              />
-            </a>
-          </li>
-          <li>
-            <a href="https://www.linkedin.com/in/umair-ahmed-0222b377/">
-              <i
-                className="fa fa-linkedin icon-2x"
-                aria-hidden="true"
-                title="Linkedin"
-              />
-            </a>
-          </li>
-          <li>
-            <a href="https://github.com/ahmedu007/FE-NC_News">
-              <i
-                className="fa fa-github icon-2x"
-                aria-hidden="true"
-                title="GitHub"
-              />
-            </a>
-          </li>
-          <li style={{ width: "150px" }}>
-            <a href="https://bulma.io/">
-              <img
-                src="https://bulma.io/images/made-with-bulma.png"
-                alt="footer"
-                title="Bulma"
-              />
-            </a>
-          </li>
-        </ul>
-      </nav>
-    </footer>
+    <Segment inverted vertical style={{ margin: "5em 0em 0em" }}>
+      <Container textAlign="center">
+        <Divider inverted section />
+        <List horizontal inverted divided link>
+          <List.Item as="a" href="https://twitter.com/umair170">
+            <Icon name="twitter" size="big" title="Twitter" />
+          </List.Item>
+          <List.Item
+            as="a"
+            href="https://www.linkedin.com/in/umair-ahmed-0222b377/"
+          >
+            <Icon name="linkedin" size="big" title="Linkedin" />
+          </List.Item>
+          <List.Item as="a" href="https://github.com/ahmedu007/FE-NC_News">
+            <Icon name="github" size="big" title="GitHub" />
+          </List.Item>
+        </List>
+      </Container>
+    </Segment>
   );
 };
 

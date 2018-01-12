1602309fe1c00017f80cb172366f2cbc12c7f7a1
ahmedu007 <umair170@hotmail.com>
2018-01-12 14:00:16 +0000

    Changing font and color

--- src/components/ArticlesList.js
+++ src/components/ArticlesList.js
@@ -26,53 +26,8 @@ const ArticlesList = props => {
           </NavLink>
         </Grid.Column>
       </Grid.Row>
-
       <Grid.Row />
     </Grid>
-
-    // <Grid columns="equal">
-    //   <Grid.Column>
-    //     <Segment>
-    //       <VoteUpDown type="articles" votes={article.votes} id={article._id} />
-    //     </Segment>
-    //   </Grid.Column>
-    //   <Grid.Column width={10}>
-    //     <NavLink to={`/articles/${article._id}`} key={article._id}>
-    //       <Segment style={{ height: "100%" }} className="article-hover">
-    //         {article.title}
-    //         <p>
-    //           <em>
-    //             by: <strong>{article.created_by}</strong>
-    //           </em>
-    //         </p>
-    //       </Segment>
-    //     </NavLink>
-    //   </Grid.Column>
-    // </Grid>
-
-    // <div className="columns">
-    //   <div className="column">
-    //     <div className="box" style={{ height: "100%" }}>
-    //       <VoteUpDown type="articles" votes={article.votes} id={article._id} />
-    //     </div>
-    //   </div>
-    //   <div className="column is-three-quarters">
-    //     <div className="box" style={{ height: "100%", fontSize: "1.2rem" }}>
-    //       <NavLink to={`/articles/${article._id}`} key={article._id}>
-    //         {article.title}
-    //         <br />
-    //         <br />
-    //         <p>
-    //           <small>
-    //             <em>
-    //               by: <strong>{article.created_by}</strong>
-    //             </em>
-    //           </small>
-    //         </p>
-    //       </NavLink>
-    //     </div>
-    //   </div>
-    // </div>
   );
 };
 
